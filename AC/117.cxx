7e3bd8db8c67e88d2c0a312c6de9a450e65dcfb1
ichoyjx <ichoyjx1106@gmail.com>
2013-09-23 04:26:35 -0500

    117

---
+++ AC/117.cxx
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int pow_mod(int base, int pow, int mod) {
+  int reminder;
+  if (pow == 1) {
+    return base % mod;
+  }
+  reminder = pow_mod(base, pow/2, mod);
+  reminder = reminder * reminder % mod;
+
+  if (pow % 2) {
+    return reminder * base % mod;
+  } else {
+    return reminder;
+  }
+}
+
+int main() {
+  int N, M, K;
+  scanf("%d%d%d", &N, &M, &K);
+
+  int i, num, count=0;
+  for (i=0; i<N; i++) {
+    scanf("%d", &num);
+    if (pow_mod(num, M, K) == 0) {
+      count++;
+    }
+  }
+
+  printf("%d\n", count);
+  return 0;
+}
