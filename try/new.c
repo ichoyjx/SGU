40ec1d4fea26461b91bf80394c8520a8a86ec2fd
ichoyjx <ichoyjx1106@gmail.com>
2013-09-22 22:52:20 -0500

    new 119

---
+++ try/new.c
@@ -0,0 +1,8 @@
+/*
+ * Given N and pair (A', B'),
+ * find all pairs (A, B) that:
+ *    for any integer X and Y,
+ *       if A'X + B'Y is divided by N
+ *       then AX + BY is divided by N too (0<= A,B <N).
+ *
+ */
