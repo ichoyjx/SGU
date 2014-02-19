007862987d406fc4e76860c0c861bd316265276c
Brian Yang <brianyang1106@gmail.com>
2014-02-19 00:04:55 -0600

    find the last rope

--- try/545.cxx
+++ try/545.cxx
@@ -1,8 +1,22 @@
-// idea: simulation + 暴搜 (exhaustive search)
+/* idea: simulation + 暴搜 (exhaustive search)
+ *       think... reversely!
+ */
 #include <iostream>
 #include <malloc.h>
 
+#define PRECISION 3
+#define N_MAX 20
+
 using namespace std;
+
+int pow (int base, int exponent) {
+  int i;
+  for (i=0; i<exponent-1; i++) {
+    base *= base;
+  }
+
+  return base;
+}
 
 int main() {
   short i;
@@ -24,11 +38,27 @@ int main() {
   for (i=0; i<n; i++)
     cin >> l[i];
 
-  for (i=0; i<n-1; i++)
+  short rope_tracker[N_MAX+1] = {0};
+  for (i=0; i<n-1; i++) {
     cin >> p[i];
+    rope_tracker[p[i]] = 1;
+  }
 
   for (i=0; i<m; i++)
     cin >> t[i];
+
+  /* find the last rope connects to the ball */
+  for (i=1; i<n; i++) {
+    if (rope_tracker[i] == 0) break;
+  }
+  short start = i; // reversely, will be the start point
+
+  cout << i << endl;
+
+  /* initialize time tracker */
+  int t_max = t[m-1] * pow (10, PRECISION);
+  float *pos;
+  pos = (float *) malloc (t_max * t_max * sizeof(float));
 
   return 0;
 }
